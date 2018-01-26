f2748c6c0d125bf0933a81942899dfbf1f78048c
Jamie Luckett <jamieluckett@googlemail.com>
2018-01-26 01:17:44 +0000

    Should have been in last push
    oops

--- eventually/event/models.py
+++ eventually/event/models.py
@@ -9,12 +9,14 @@ import string
 # Event Object/Model Definitions
 
 class Event(models.Model):	
+	"""Defines Event table"""
 	event_name = models.CharField(max_length=50, help_text="Enter Event name")
 	event_date = models.DateTimeField(help_text="Enter Event Date and Time")
 	event_date_created = models.DateTimeField(default=datetime.now, blank=True)
 	event_key = models.CharField(max_length=constants.KEY_LENGTH)
 		
 	def __str__(self):
+		"""Overwrites the models string return to make admin view pretty"""
 		return "%s - %s" % (str(self.id), self.event_name[:30])
 		#ID - Name
 		
@@ -22,14 +24,33 @@ class Event(models.Model):
 		"""Generates a key of length KEY_LENGTH"""
 		key = ""
 		for n in range(constants.KEY_LENGTH):
-			key.join(SystemRandom().choice(string.ascii_uppercase + string.digits)
+			key.join(SystemRandom().choice(string.ascii_uppercase + string.digits))
 		return key
 	
 class Guest(models.Model):
+	"""Defines Guest table"""
 	first_name = models.CharField(max_length=30, help_text="Enter Guest first name")
 	last_name = models.CharField(max_length=30, help_text="Enter Guest surname")
 	email_models = models.EmailField(max_length=70, help_text="Enter Guest email address")
 	
-	def __str__(self): #overwrite string return to pretty up admin panels
+	def __str__(self):
+		"""Overwrites the models string return to make admin view pretty"""
 		return "%s - %s. %s" % (str(self.id), self.first_name[0], self.last_name)
 		#ID - A. Surname
+		
+class EventLine(models.Model):
+	"""Defines abstract EventLine table.
+	Connects an Event to a Guest"""
+	event_id = models.ForeignKey(Event, on_delete=models.CASCADE)
+	guest_id = models.ForeignKey(Guest, on_delete=models.CASCADE)
+	
+	def __str__(self):
+		"""Overwrites the models string return to make admin view pretty"""
+		return "E%s/G%s"%(str(self.event_id.id), str(self.guest_id.id))
+		
+	class Meta:
+		verbose_name_plural = "Pairings"
+		
+class EventPassword(models.Model):
+	event_id = models.ForeignKey(Event, on_delete=models.CASCADE)
+	hashed_password = models.CharField(max_length=256)
--- eventually/eventually/settings.py
+++ eventually/eventually/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 	'event',
-]
+] #List of apps Django will use
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'eventually.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
